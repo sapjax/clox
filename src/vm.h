0bb561e78b7f0cffd1d1ca4dd4d61d1c51269ae1
sapjax <sapjax@gmail.com>
2023-12-20 17:21:22 +0800

    a-virtual-machine

---
+++ src/vm.h
@@ -0,0 +1,27 @@
+#ifndef __VM__H__
+#define __VM__H__
+
+#include "chunk.h"
+
+#define STACK_MAX 256
+
+typedef struct {
+  Chunk* chunk;
+  uint8_t* ip;
+  Value stack[STACK_MAX];
+  Value* stackTop;
+} VM;
+
+typedef enum {
+  INTERPRET_OK,
+  INTERPRET_COMPILE_ERROR,
+  INTERPRET_RUNTIME_ERROR,
+} InterpretResult;
+
+void initVM();
+void freeVM();
+InterpretResult interpret(Chunk* chunk);
+void push(Value value);
+Value pop();
+
+#endif  //!__VM__H__
